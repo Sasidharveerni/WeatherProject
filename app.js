131643512c0f1180a2fbedb06a79cf48951130a3
Sasidhar Veerni <81133052+Sasidharveerni@users.noreply.github.com>
2023-08-04 23:23:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post("/", function(req, res) {
           <meta charset="utf-8">
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         </head>
-        <body style = 'background-image: url(images/weather-img.jpg); background-repeat: no-repeat;
+        <body style = 'background-image: url(https://cdn.glitch.global/2d314d96-5201-491d-ac70-4515d0bb864d/weather-img.jpg?v=1685691280603); background-repeat: no-repeat;
         background-size: 100% auto;' >
             <div class="container">
               <div class="row justify-content-center mt-5">
